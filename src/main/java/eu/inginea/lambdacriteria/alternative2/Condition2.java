52173274a2803102fa818b234b74bb739d9ac91a
ondro <ondro@ondro-lenovo-flex>
2016-02-13 19:29:11 +0100

    Removed uncompilable alternatives - intention to try concept with Streams

--- src/main/java/eu/inginea/lambdacriteria/alternative2/Condition2.java
+++
@@ -1,8 +0,0 @@
-package eu.inginea.lambdacriteria.alternative2;
-
-import java.io.Serializable;
-import java.util.function.Predicate;
-
-public interface Condition2<PARAM> extends Predicate<PARAM>, Serializable {
-
-}
