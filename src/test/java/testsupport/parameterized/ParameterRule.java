551a7dbace72e41a527bf0466dff8c4b0d4deec6
ondro <ondro@ondro-lenovo-flex>
2016-02-27 15:14:56 +0100

    Improved ParameterRule to execute test multiple times only when parameter is used inside the test

--- src/test/java/testsupport/parameterized/ParameterRule.java
+++ src/test/java/testsupport/parameterized/ParameterRule.java
@@ -1,6 +1,7 @@
 package testsupport.parameterized;
 
 import java.util.*;
+import org.junit.AssumptionViolatedException;
 import org.junit.rules.*;
 import org.junit.runner.Description;
 import org.junit.runners.model.*;
@@ -9,6 +10,7 @@ public class ParameterRule<PARAM_TYPE> implements TestRule {
 
     private final Iterable<PARAM_TYPE> parameters;
     private PARAM_TYPE currentParam;
+    private boolean paramUsedInTest;
     Iterator<PARAM_TYPE> itParams;
 
     public ParameterRule(Iterable<PARAM_TYPE> parameters) {
@@ -16,8 +18,13 @@ public class ParameterRule<PARAM_TYPE> implements TestRule {
     }
 
     public PARAM_TYPE getParameter() {
-        if (currentParam == null) {
-            currentParam = itParams.next();
+        if (!paramUsedInTest) {
+            paramUsedInTest = true;
+            if (itParams.hasNext()) {
+                currentParam = itParams.next();
+            } else {
+                throw new AssumptionViolatedException("Skipping this test as no parameter left in the list, probably the list of parameters is empty");
+            }
         }
         return currentParam;
     }
@@ -29,25 +36,43 @@ public class ParameterRule<PARAM_TYPE> implements TestRule {
             @Override
             public void evaluate() throws Throwable {
 
-                List<Throwable> errors = new ArrayList<Throwable>();
+                List<Throwable> errors = new ArrayList<>();
                 itParams = parameters.iterator();
                 int index = 0;
 
-                while (itParams.hasNext()) {
-                    currentParam = null;
-                    try {
-                        stmnt.evaluate();
-                    } catch (Throwable ex) {
+                paramUsedInTest = false;
+                try {
+                    stmnt.evaluate();
+                } catch (Throwable ex) {
+                    if (!paramUsedInTest) {
+                        throw ex;
+                    } else {
                         addError(errors, ex, index);
                     }
-                    index++;
                 }
 
-                MultipleFailureException.assertEmpty(errors);
+                if (paramUsedInTest) {
+                    while (itParams.hasNext()) {
+                        index++;
+                        paramUsedInTest = false;
+                        try {
+                            stmnt.evaluate();
+                        } catch (Throwable ex) {
+                            if (paramUsedInTest) {
+                                addError(errors, ex, index);
+                            }
+                        }
+                        if (!paramUsedInTest) {
+                            throw new IllegalStateException("Parameterized test does not use parameter. You should not skip getting parameter for some tests");
+                        }
+                    }
+                    MultipleFailureException.assertEmpty(errors);
+                }
+
             }
 
             private boolean addError(List<Throwable> errors, Throwable ex, int index) {
-                return errors.add(new AssertionError("(parameter " + index + ") " 
+                return errors.add(new AssertionError("(parameter " + index + ") "
                         + ex.getMessage(), ex));
             }
         };
--- src/test/java/testsupport/parameterized/ParameterizedRuleTest.java
+++ src/test/java/testsupport/parameterized/ParameterizedRuleTest.java
@@ -1,24 +1,71 @@
 package testsupport.parameterized;
 
-import java.util.Arrays;
+import java.util.*;
+import static org.hamcrest.Matchers.*;
 import org.junit.*;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import org.junit.rules.ExpectedException;
 
 public class ParameterizedRuleTest {
-    
+
+    private static int testCounter = 0;
+
+    private static final List<String> STRING_PARAMS = Arrays.asList("1", "2");
+    private static final List<String> OTHER_STRING_PARAMS = Arrays.asList("s1", "s2", "s3");
+
     @Rule
-    public ParameterRule<String> strings = new ParameterRule<>(Arrays.asList("1", "2"));
-    
+    public ParameterRule<String> strings = new ParameterRule<>(STRING_PARAMS);
+
+    @Rule
+    public ParameterRule<String> otherStrings = new ParameterRule<>(OTHER_STRING_PARAMS);
+
     @Rule
     public ExpectedException expected = ExpectedException.none();
-    
+
+    @Before
+    public void countTest() {
+        testCounter++;
+    }
+
     @Test
     public void canRunTestWithParameters() {
         String s = strings.getParameter();
         expected.expect(AssertionError.class);
-        
+
         fail("Failing for " + s);
+    }
+
+    @Test
+    public void canRunTestWithOtherParameters() {
+        String s = otherStrings.getParameter();
+        expected.expect(AssertionError.class);
+
+        fail("Failing for " + s);
+    }
+
+    @Test
+    public void canRunTestWithCombinationOfParameters() {
+        String s = strings.getParameter();
+        String otherS = otherStrings.getParameter();
+        expected.expect(AssertionError.class);
+
+        fail("Failing for s=" + s + ", other=" + otherS);
+    }
+
+    @Test
+    public void canRunTestWithoutParameters() {
+        expected.expect(AssertionError.class);
+
+        fail("Failing some test");
+    }
+
+    @AfterClass
+    public static void canRunAllTestsAsSupposed() {
+        assertThat(testCounter, is(equalTo(
+                STRING_PARAMS.size()
+                + OTHER_STRING_PARAMS.size()
+                + (STRING_PARAMS.size() * OTHER_STRING_PARAMS.size())
+                + 1)));
     }
 
 }
